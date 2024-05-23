d6f7bfeec6ab6c2024a856092c5ce945a38fb98d
Shakti Singh <sk.shrinet@gmail.com>
2024-05-23 12:24:28 +0530

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Aisle from './app/Aisle';
 import reportWebVitals from './reportWebVitals';
+import { StrictMode } from 'react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <Aisle />
-  </React.StrictMode>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
